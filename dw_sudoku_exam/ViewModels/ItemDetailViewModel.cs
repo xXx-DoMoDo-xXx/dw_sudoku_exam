d896310491a2bbe16f55808f73c37587ed02f07b
DoMoDo <dw@cabtronix.ch>
2020-01-11 12:42:27 +0100

    initial project
    Signed-off-by: DoMoDo <dw@cabtronix.ch>

---
+++ dw_sudoku_exam/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using dw_sudoku_exam.Models;
+
+namespace dw_sudoku_exam.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ dw_sudoku_exam_test/dw_sudoku_exam_test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dw_sudoku_exam_test
+{
+    [TestClass]
+    public class dw_sudoku_exam_test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
